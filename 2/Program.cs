c50d8bded437fd64b80029f42bae60768385e271
Arina Nikitina <71166058+arinanikitina@users.noreply.github.com>
2021-11-19 21:24:39 +0300

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string s1 = "beatiful sea";
+            string s2 = "aes lufitaeb";
+            string s3 = "Beatiful beach";
+            int m = 1;
+            char[] arr = s1.ToCharArray();
+            char[] arr2 = s3.ToCharArray();
+            Array.Reverse(arr2);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != arr2[i])
+                {
+                    Console.WriteLine("False");
+                    m = 0;
+                    break;
+                }
+            }
+
+            if (m == 1) Console.WriteLine("Vse ok");
+        }
+    }
+}
