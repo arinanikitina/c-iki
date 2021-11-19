8efb5a4498661b73271f7ea90eb401abeb3bd968
Arina Nikitina <71166058+arinanikitina@users.noreply.github.com>
2021-11-19 19:26:42 +0300

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 3, 4, 5};
+            int[] numbers2 = {6, 7, 8, 9, 10};
+            string a;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    a = numbers[i].ToString() + numbers2[j].ToString();
+                    if (Convert.ToInt32(a) % 5 == 0) Console.WriteLine(Convert.ToInt32(a));
+
+                }
+            }
+
+        }
+    }
+}
